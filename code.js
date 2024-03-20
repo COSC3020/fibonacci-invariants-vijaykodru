8de3e9bb2f32a5661a8d42bb04966973800aefd9
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-03-20 05:19:20 +0000

    Final Code

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function fib(n) {
-    return n;
+    if(n === 0){
+        return [0];
+    }
+    if(n === 1){
+        return[0,1]
+    }
+    else{
+        var a = fib(n-1);
+        a.push(a[n-1]+a[n-2]);
+        return a;
+    }
 }
